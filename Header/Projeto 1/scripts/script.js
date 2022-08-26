7152600fb92737d40bd1b5fec9e7d51151713974
ntn-ss <91851915+ntn-ss@users.noreply.github.com>
2022-08-26 12:20:29 -0300

    Commit

--- Header/Projeto 1/scripts/script.js
+++ Header/Projeto 1/scripts/script.js
@@ -4,6 +4,7 @@ const $header = document.querySelector("header");
 window.addEventListener("scroll", toggleHeader, false);
 
 const $logo = document.querySelectorAll('.logo')[0];
+const $imagem = document.getElementById("imagem");
 
 const $nav = document.querySelectorAll('.nav-bar')[0];
 $nav.addEventListener("click", navClick, false);
@@ -16,9 +17,9 @@ function toggleHeader(){
         $header.classList.remove("max-header");
         $header.classList.add("min-header");
         
-        $logo.firstElementChild.setAttribute('src','../Projeto 1/imgs/Test4v2.png');
         $logo.classList.remove("max-logo");
         $logo.classList.add("min-logo");
+        $imagem.setAttribute('src','../Projeto 1/imgs/Test4v2.png');
 
         $nav.classList.remove("max-nav");
         $nav.classList.add("min-nav");
@@ -30,9 +31,9 @@ function toggleHeader(){
         $header.classList.add("max-header");
         $header.classList.remove("min-header");
         
-        $logo.firstElementChild.setAttribute('src','../Projeto 1/imgs/Test4.png');
-        $logo.classList.add("max-logo");
         $logo.classList.remove("min-logo");
+        $logo.classList.add("max-logo");
+        $imagem.setAttribute('src','../Projeto 1/imgs/Test4.png');
 
         $nav.classList.add("max-nav");
         $nav.classList.remove("min-nav");
