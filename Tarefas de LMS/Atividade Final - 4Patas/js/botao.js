4347b413acf4651f54ce69ba35eda6b1db4d4049
ntn-ss <91851915+ntn-ss@users.noreply.github.com>
2022-11-28 09:23:29 -0300

    Site 4 Patas

--- Tarefas de LMS/Atividade Final - 4Patas/js/botao.js
+++ Tarefas de LMS/Atividade Final - 4Patas/js/botao.js
@@ -1,7 +1,7 @@
 const toTop = document.querySelector(".toTop");
 
 window.addEventListener("scroll", () => {
-    if (window.pageYOffset > 30) {
+    if (window.pageYOffset > 30 && window.pageYOffset < 8850) {
         toTop.classList.add("active");
     } else {
         toTop.classList.remove("active");
