4ef1d2a4b311cb5c040f412a561a12fc619b1edb
ntn-ss <nathandossantosdasilva@gmail.com>
2024-05-24 12:01:39 -0300

    v2 da ROM

--- Projetos_de_Traducao/DBZ-SGTH/scripts/script.js
+++ Projetos_de_Traducao/DBZ-SGTH/scripts/script.js
@@ -3,7 +3,7 @@ let elImagem = document.getElementById('random');
 let numero = 0;
 
 const imagemAleatoria = () => {
-    numero = Math.floor(Math.random() * 39);
+    numero = Math.floor(Math.random() * 114);
     elImagem.setAttribute('src', `./assets/Prints_Randomizadas/${numero}.jpg`);
 }
 
